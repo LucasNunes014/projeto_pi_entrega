3911ec994580db9e41fc19f3badb8ccb8e5fd0d4
Lucas Daniel Nunes <lucas.nunes34@fatec.sp.gov.br>
2025-10-30 20:26:05 -0300

    Clonando Projeto

---
+++ app/Views/home.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Locadora LK</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:400,400i,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+  <code> php -S 127.0.0.1:8000 -t public  </code>
+    <br />
+
+	<div class="superinfo-bg">
+		<div class="superinfo">
+			<p>Seg / Sex - 08:00 às 18:00</p>
+			<a href="tel:+552199999999">+55 14 99867-5053</a>
+			<p>Av. Ali Perto, 330, Jau - SP</p>
+		</div>
+	</div>
+	
+	<header class="menu-bg">
+		<div class="menu">
+			<div class="menu-logo">
+				<a href="#">Locadora LK</a>
+			</div>
+			<nav class="menu-nav">
+				<ul>
+					<li><a href="#sobre">Sobre</a></li>
+					<li><a href="#produtos">Filmes</a></li>
+					<li><a href="#preco">Preço</a></li>
+					<li><a href="#qualidade">Catálogos</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	
+	<h1 class="introducao">Encontre Seus Filmes e<br>Jogos Favoritos</h1>
+	
+	<section class="sobre" id="sobre">
+		<div class="sobre-info">
+			<h1>Sobre</h1>
+			<p>Bem-vindo à Locadora LK!, sua melhor escolha para locação de filmes e
+          jogos! Criamos um espaço online pensado para proporcionar uma
+          experiência única de entretenimento, com um acervo diversificado de
+          filmes, séries e jogos para todos os gostos e idades.</p>
+		</div>
+		<div class="sobre-img">
+			<img src="/imgs/sobre1.jpg" alt="Sobre 1">
+		</div>
+		<div class="sobre-img">
+			<img src="/imgs/SOBRE2.jpg" alt="Sobre 2">
+		</div>
+	</section>
+	
+	<section class="produtos" id="produtos">
+		<h1>Produtos</h1>
+		<div class="produtos-container">
+			<div class="produtos-item purple">
+				<h2>Filmes</h2>
+				<img src="/imgs/filmes.jpg" alt="Filmes 1">
+			</div>
+			<div class="produtos-item pink">
+				<h2>Jogos</h2>
+				<img src="/imgs/jogos.jpg" alt="Jogos 1">
+			</div>
+			<div class="produtos-item blue">
+				<h2>Alugue Já</h2>
+				<img src="/imgs/Alugue.jpg" alt="Alugue 1">
+			</div>
+		</div>
+	</section>
+	
+	<section class="preco" id="preco">
+		<div class="preco-item">
+			<h2>Filmes</h2>
+			<span><sup>R$</sup>70</span>
+			<ul>
+				<li>Preços Competitivos</li>
+				<li>Catálogo Exclusivo</li>
+				<li>Sem Taxas de Atraso</li>
+				<li>Alta Qualidade de Imagem e Som</li>
+			</ul>
+			<a href="#">Comprar</a>
+		</div>
+		
+		<div class="preco-item">
+			<h2>Jogos</h2>
+			<span><sup>R$</sup>40</span>
+			<ul>
+				<li>Ampla Variedade de Títulos</li>
+				<li>Descontos Exclusivos</li>
+				<li>Suporte Especializado</li>
+				<li>Novidades e Lançamentos</li>
+				<li>Compra Exclusiva</li>
+			</ul>
+			<a href="#">Comprar</a>
+		</div>
+		
+		<div class="preco-item">
+			<h2>Alugar</h2>
+			<span><sup>R$</sup>20</span>
+			<ul>
+				<li>Facilidade de Locação Online</li>
+				<li>Sem Compromisso de Compra</li>
+				<li>Catálogo Atualizado</li>
+				<li>Facilidade de Devolução</li>
+				<li>Compra Exclusiva</li>
+				<li>Download dos Itens</li>
+			</ul>
+			<a href="#">Comprar</a>
+		</div>
+	</section>
+	
+	<section class="qualidade" id="qualidade">
+		<div class="qualidade-item">
+			<h2>Facilidade e Comodidade</h2>
+			<p>Alugue filmes de forma simples e rápida, diretamente online, com entrega em casa.</p>
+		</div>
+		
+		<div class="qualidade-item">
+			<h2>Variedade de Títulos</h2>
+			<p>Um catálogo amplo, com opções para todos os gostos e gêneros.</p>
+		</div>
+		
+		<div class="qualidade-item">
+			<h2>Preços Justos</h2>
+			<p>Planos acessíveis e transparentes, sem taxas escondidas.</p>
+		</div>
+		
+		<div class="qualidade-item">
+			<h2>Sem Compromisso de Compra</h2>
+			<p>Alugue por um período e devolva sem a necessidade de comprar.</p>
+		</div>
+		
+		<div class="qualidade-item">
+			<h2>Catálogo Sempre Atualizado</h2>
+			<p>Novidades e lançamentos fresquinhos, para você estar sempre por dentro dos maiores sucessos.</p>
+		</div>
+		
+		<div class="qualidade-item">
+			<h2>Sem Taxas de Atraso</h2>
+			<p>Alugue filmes com flexibilidade, sem se preocupar com multas ou taxas por atraso na devolução.</p>
+		</div>
+		
+	</section>
+	
+	<section class="newsletter">
+		<div class="newsletter-info">
+			<h1>Newsletter</h1>
+			<p>Acesse e fique por dentro das novidades: filmes e jogos exclusivos, entregues com rapidez e qualidade!</p>
+		</div>
+		<form class="newsletter-form">
+			<input type="text" placeholder="Seu e-mail">
+			<button type="submit">Acessar</button>
+		</form>
+	</section>
+	
+	<footer class="footer">
+		<p>Locadora LK © Todos os direitos reservados.</p>
+	</footer>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+// Importa o autoload do composer para carregar as rotas
+require __DIR__ . '/../vendor/autoload.php';
+
+// Obtem a URL do navegador
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+echo $url;
+
+if ($url == "/") {
+    require __DIR__ . '/../app/Views/home.php';
+}
